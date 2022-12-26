3d97e6ea37b33ca408463f7262149797f4914658
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-26 22:40:24 +0300

    feat: загрузить из ветки main работу 15 спринт

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,10 @@ const handlerCors = require('./middlewares/handlerCors');
 const PORT = 3000;
 const app = express();
 
-app.use(requestLogger);
-app.use(handlerCors);
 
 app.use(express.json());
+app.use(requestLogger);
+app.use(handlerCors);
 app.use(routes);
 
 app.use(errorLogger);
