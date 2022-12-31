9f93dfabc7ec342a96470e2c2e10da4210b8ce3c
Minin Vitaliy <mini.vitali@yandex.ru>
2022-12-31 15:43:12 +0300

    feat: загрузить из ветки main работу 15 спринт

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(requestLogger);
 app.use(handlerCors);
-
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(routes);
 
 app.use(errorLogger);
